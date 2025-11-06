2a9bfcd44c9f609d4bf88ac3da1058e0b2b26a07
JosephCouprie <joseph.couprie@centria.fi>
2025-11-06 19:51:19 +0100

    Add files via upload

--- task-g/index.js
+++ task-g/index.js
@@ -13,6 +13,63 @@ document.addEventListener("DOMContentLoaded", () => {
   const PhoneNumber_Input = document.getElementById("PhoneNumber");
   const DateofBirth_Input = document.getElementById("DateofBirth");
 
+  function checkName() {
+    const FullName = FullName_Input.value.trim();
+    if (!FullName) {
+      document.getElementById("name_error").innerHTML = "Please enter your fullname";
+      return;
+    }
+    if (!FullName.includes(" ") || FullName.split(" ")[0].length < 2 || FullName.split(" ")[1].length < 2) {
+      document.getElementById("name_error").innerHTML = "Please enter your FULL fullname";
+      return;
+    }
+    document.getElementById("name_error").innerHTML = "";
+  }
+  FullName_Input.addEventListener("input", checkName);
+
+  function checkEmail() {
+    const Email = Email_Input.value.trim();
+    if (!Email) {
+      document.getElementById("email_error").innerHTML = "Please enter your email";
+      return;
+    }
+    if (!Email.includes("@") || !Email.split("@")[1].includes(".")) {
+      document.getElementById("email_error").innerHTML = "Please a valid email (containing '@' and '.')";
+      return;
+    }
+    document.getElementById("email_error").innerHTML = "";
+  }
+  Email_Input.addEventListener("input", checkEmail);
+
+  function checkNumber() {
+    const PhoneNumber = PhoneNumber_Input.value.trim();
+    if (!PhoneNumber) {
+      document.getElementById("tel_error").innerHTML = "Please enter your phone number";
+      return;
+    }
+    if (PhoneNumber.length < 8) {
+      document.getElementById("tel_error").innerHTML = "Please a valid phone number (at least 8 characters)";
+      return;
+    }
+    document.getElementById("tel_error").innerHTML = "";
+  }
+  PhoneNumber_Input.addEventListener("input", checkNumber);
+
+  function checkBirth() {
+    const DateofBirth = DateofBirth_Input.value.trim();
+    if (!DateofBirth) {
+      document.getElementById("date_error").innerHTML = "Please enter your birth date";
+      return;
+    }
+    if (DateofBirth.value < 13 || DateofBirth.value > 130) {
+      document.getElementById("date_error").innerHTML = "Please enter a valid birth date (you must be at least 13yo and younger than 130yo";
+      return;
+    }
+  }
+  DateofBirth_Input.addEventListener("input", checkBirth);
+
+
+
   form.addEventListener("submit", (event) => {
     HiddenTimestamp_Input.value = new Date();
 
@@ -21,36 +78,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const HiddenTimestamp = HiddenTimestamp_Input.value.trim();
     if (!HiddenTimestamp) {
       return;
-    } 
+    }
 
-    const FullName = FullName_Input.value.trim();
-    if (!FullName) {
-      document.getElementById("name_error").innerHTML = "Please enter your fullname";
-      return;
-    }
-    document.getElementById("name_error").innerHTML = "";
-    
-    const Email = Email_Input.value.trim();
-    if (!Email) {
-      document.getElementById("email_error").innerHTML = "Please enter your email";
-      return;
-    }
-    document.getElementById("email_error").innerHTML = "";
-
-    const PhoneNumber = PhoneNumber_Input.value.trim();
-    if (!PhoneNumber) {
-      document.getElementById("tel_error").innerHTML = "Please enter your phone number";
-      return;
-    }
-    document.getElementById("tel_error").innerHTML = "";
-
-    const DateofBirth = DateofBirth_Input.value.trim();
-    if (!DateofBirth) {
-      document.getElementById("date_error").innerHTML = "Please enter your birth date";
-      return;
-    }
-    document.getElementById("date_error").innerHTML = "";
-  
     // Create new table row
     const row = document.createElement("tr");
 
@@ -68,7 +97,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const Email_cell = document.createElement("td");
     Email_cell.textContent = Email;
     row.appendChild(Email_cell);
-  
+
     // PhoneNumber cell
     const PhoneNumber_cell = document.createElement("td");
     PhoneNumber_cell.textContent = PhoneNumber;
